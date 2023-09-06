2249dcb39015cff1be37ce4cb608a540dbe91178
jocelyncpiano <144284964+jocelyncpiano@users.noreply.github.com>
2023-09-06 21:34:46 +0000

    save test file

---
+++ test.py
@@ -0,0 +1,5 @@
+print("Hello world")
+print("Fred went to the store.", end="***")
+print("Fred bought", "eggs", "bacon", "milk", "\n")
+print("Fred asked asked, \'How much does this cost?\'")
+print("About 5 \t\t\t\t dollars")
